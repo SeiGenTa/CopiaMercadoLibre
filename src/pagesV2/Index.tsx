86c13b5e122d019fc311dd947af63f80e0a676d4
AlMartinez01 <almartinez@ug.uchile.cl>
2024-09-27 00:09:58 -0300

    aa

--- src/pagesV2/Index.tsx
+++ src/pagesV2/Index.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import LayoutInitPage from "../layouts/layoutInitPage";
+import LayoutInitPage from "../layouts/layoutInitPage.tsx";
 import Advertising from "./sections_init_age.tsx/advertising";
 
 const Index = () => {
