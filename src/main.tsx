621140b68c87afc07284f6a5f4e96f270aaf3e9a
AlMartinez01 <almartinez@ug.uchile.cl>
2024-09-27 00:22:44 -0300

    asd

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,7 @@ import "./index.css";
 
 import { createRoot } from "react-dom/client";
 import { ToastContainer } from "react-toastify";
-import Index from "./pagesV2/Index";
+import Index from "./pagesV2/index";
 
 const App = () => {
   return (
